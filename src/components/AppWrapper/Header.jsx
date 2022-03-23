7a33afa2a3d168e611f5933eb5d84dd5574e271e
maheshvarpandey <mkp10697@gmail.com>
2022-03-23 16:30:31 +0530

    added the routing

--- src/components/AppWrapper/Header.jsx
+++ src/components/AppWrapper/Header.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
 
@@ -34,7 +35,11 @@ export default function Header() {
                     </div>
                     <div className="hidden sm:flex flex-row space-x-4">
                         {/* <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-indigo-700 bg-white border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">Sign Up</button> */}
-                        <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">Contact US</button>
+                        <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
+                           <Link to={"contact"}>
+                                Contact US
+                            </Link>        
+                        </button>
                     </div>
                     {/* Burger Icon */}
                     <div id="bgIcon" onClick={()=>setshow(!show)} className={`focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800  justify-center items-center sm:hidden cursor-pointer`}>
@@ -71,7 +76,11 @@ export default function Header() {
                     </div>
                     <div className="flex flex-col gap-4 mt-4 w-80 mx-auto ">
                         {/* <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-indigo-700 bg-indigo-600 bg-opacity-0 hover:opacity-100 duration-100 border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">Sign Up</button> */}
-                        <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">Contact US</button>
+                        <button className="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
+                           <Link to={"contact"}>
+                                Contact US
+                            </Link>        
+                        </button>
                     </div>
                 </div>
             </nav>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Contact from './components/Contact';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
