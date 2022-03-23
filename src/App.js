281a08c9b5b03ede1d8fb32408e28d4bd12321e8
maheshvarpandey <mkp10697@gmail.com>
2022-03-23 15:43:11 +0530

    added all the components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import AppWrapper from './components/AppWrapper/AppWrapper';
+import Blog from './components/Blog';
+import Features from './components/Features/Features';
+import List from './components/List';
+// import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from "pure-react-carousel";
+// import "pure-react-carousel/dist/react-carousel.es.css";
+// import HomePage from './components/HomePage/HomePage';
+import Pricing from './components/Pricing';
+// import Stats from './components/Stats';
+import Teams from './components/Teams';
+import Testimonial from './components/Testimonial';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <AppWrapper>
+      <header className="">
+        <List />
+        <Teams />
+        {/* <Stats /> */}
+        <Pricing />
+        <Features />
+        <Blog />
+        <div>
+            <div className="2xl:mx-auto 2xl:container md:px-20 px-4 md:py-12 py-9">
+                <div className="relative rounded-md">
+                    <img src="https://i.ibb.co/SBpL1cK/pexels-aleksandar-pasaric-325185-1.png" alt="city view" className="w-full h-full rounded-md  object-center object-fill absolute sm:block hidden" />
+                    <img src="https://i.ibb.co/LQpxBsc/mobile.png" alt="city view" className="w-full h-full rounded-md absolute object-center object-fill sm:hidden" />
+                    <div className="text-xl relative z-20 bg-gradient-to-r from-blue-700 to-transparent w-full h-full z-40 top-0 md:p-16 p-6 flex flex-col justify-between rounded-md ">
+                        <div>
+                            <h1 className="md:text-5xl text-3xl font-bold md:leading-10 leading-9 text-white sm:w-auto w-64">Act Before It’s Too Late!</h1>
+                            <p className="text-lg leading-6 text-white xl:w-5/12 lg:w-8/12 md:w-10/12  2xl:pr-12 mt-4">A good idiom for kids is "It's raining cats and dogs." Kids know what both cats and dogs are from an early age.</p>
+                        </div>
+                        <div className="md:mt-12 mt-20">
+                            <button className="text-base font-medium leading-4 text-indigo-700 bg-white sm:w-auto w-full rounded p-4 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-white">Explore More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Testimonial />
       </header>
-    </div>
+    </AppWrapper>
   );
 }
 
